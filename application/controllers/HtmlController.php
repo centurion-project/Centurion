b8a3ee5e9c01f0fdb5af702bb13ae1b7398a1370
Laurent <lc@centurion-project.org>
2012-02-07 17:28:51 +0100

    Fix HtmlController.php listing.

--- application/controllers/HtmlController.php
+++ application/controllers/HtmlController.php
@@ -25,7 +25,7 @@ class HtmlController extends Centurion_Controller_Action
             $method = $methodClass->name;
 
             if ('_' !== $method[0] && 'indexAction' !== $method && 'Action' === substr($method, -6)) {
-                $htmlMethod[] = $method;
+                $htmlMethod[] = substr($method, 0, -6);
             }
         }
 
