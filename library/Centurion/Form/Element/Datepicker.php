f19bcab5f70a1efae527b910c2c8bd3aff594a40
Laurent <lc@centurion-project.org>
2012-03-22 18:57:42 +0100

    Create element DatePicker and DateTimePicker.

--- library/Centurion/Form/Element/Datepicker.php
+++ library/Centurion/Form/Element/Datepicker.php
@@ -1,6 +1,15 @@
 <?php
 
-class Centurion_Form_Element_Datepicker extends Zend_Form_Element_Text
+class Centurion_Form_Element_DatePicker extends Zend_Form_Element_Text
 {
-    
+    protected $_defaultOptions = array(
+        'class' => 'field-datepicker',
+    );
+
+    public function __construct($spec, $options = null)
+    {
+        $options = array_merge($this->_defaultOptions, $options);
+        return parent::__construct($spec, $options);
+    }
+
 }
