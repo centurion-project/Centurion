6ba1d25d7926934b967491a4c37baec11b43069c
Florent Jaby <florent.jaby@gmail.com>
2012-03-28 13:39:14 +0200

    remove debug stuff, I DID NOT PUT THAT IN HERE, I don't know what happened
    Conflicts:
    
    	library/Centurion/Db/Table/Row/Abstract.php

--- library/Centurion/Db/Table/Row/Abstract.php
+++ library/Centurion/Db/Table/Row/Abstract.php
@@ -170,9 +170,6 @@ abstract class Centurion_Db_Table_Row_Abstract extends Zend_Db_Table_Row_Abstrac
             if ($row == null)
                 return null;
 
-            throw new Exception();
-            var_dump($columnName);
-            die();
             return $row->{substr($columnName, $pos + 2)};
         }
 
