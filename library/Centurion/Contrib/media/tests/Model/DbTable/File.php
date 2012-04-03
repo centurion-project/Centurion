f883015378b7456ba6c50ac3cf87f8bd41b39864
Laurent <lc@centurion-project.org>
2012-04-03 17:28:13 +0200

    Add test unit on dbTable_File

--- library/Centurion/Contrib/media/tests/Model/DbTable/File.php
+++ library/Centurion/Contrib/media/tests/Model/DbTable/File.php
@@ -25,4 +25,41 @@ class Media_Test_Model_DbTable_File extends PHPUnit_Framework_TestCase
 
         //TODO: check that the file is 1px height and width
     }
+
+    /**
+     * Test the update of an image
+     * @covers Media_Model_DbTable_File::insert
+     */
+    public function testInsertImage()
+    {
+        $mediaRow = Centurion_Db::getSingleton('media/file')->createRow(array('local_filename' => APPLICATION_PATH . '/../library/Centurion/Contrib/media/tests/Support/images/centurion.png'));
+        $mediaRow->save();
+
+        $imageInfoArray = array(
+            'mime' => 'image/png',
+            'local_filename' => APPLICATION_PATH . '/../library/Centurion/Contrib/media/tests/Support/images/centurion.png',
+            'filename' => 'centurion.png',
+            'filesize' => '676'
+        );
+
+        $this->assertEquals($imageInfoArray['mime'], $mediaRow->mime, 'Mime type doesn\'t match');
+    }
+
+    /**
+     * Test the update of an image
+     * @covers Media_Model_DbTable_File::update
+     */
+    public function testUpdateImage()
+    {
+        $mediaRow = Centurion_Db::getSingleton('media/file')->createRow(array('local_filename' => APPLICATION_PATH . '/../library/Centurion/Contrib/media/tests/Support/images/centurion.png'));
+        $mediaRow->save();
+
+        $newMediaRow = clone $mediaRow;
+        $newMediaRow->local_filename = APPLICATION_PATH . '/../library/Centurion/Contrib/media/tests/Support/images/tank_centurion.jpg';
+        $newMediaRow->save();
+
+        $this->assertEquals(filesize(APPLICATION_PATH . '/../library/Centurion/Contrib/media/tests/Support/images/tank_centurion.jpg'), $newMediaRow->filesize);
+        $this->assertEquals('tank_centurion.jpg', $newMediaRow->filename);
+        $this->assertEquals('image/jpg', $newMediaRow->mime);
+    }
 }
