8539d8ab79f3ccc51edbf8a64d9bb5b4da6fc28a
Laurent <lc@centurion-project.org>
2012-04-06 18:26:12 +0200

    Add test unit on Centurion_Mail_Transport_Blackhole.

---
+++ tests/library/Centurion/Mail/BlackHoleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once dirname(__FILE__) . '/../../../../tests/TestHelper.php';
+
+class Centurion_Signal_BlackHoleTest extends PHPUnit_Framework_TestCase
+{
+    public function testSendMail()
+    {
+        $mail = new Zend_Mail();
+        $mail->setBodyText('This mail should never be sent.');
+        $mailTransport = new Centurion_Mail_Transport_Blackhole();
+
+        $mailTransport->send($mail);
+    }
+}
