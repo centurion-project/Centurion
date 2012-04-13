fe6a5c4bef751db08bc00359f6b66e137f881852
Laurent <lc@centurion-project.org>
2012-04-13 14:45:51 +0200

    Don't use Session in phpunit.

--- tests/library/Centurion/Auth/Adapter/DbTableTest.php
+++ tests/library/Centurion/Auth/Adapter/DbTableTest.php
@@ -5,6 +5,7 @@ require_once dirname(__FILE__) . '/../../../../../tests/TestHelper.php';
 
 /**
  * @covers Centurion_Auth_Adapter_DbTable
+ * @covers Auth_Form_Validator_Login
  */
 class Centurion_Auth_Adapter_DbTableTest extends PHPUnit_Framework_TestCase
 {
@@ -12,6 +13,8 @@ class Centurion_Auth_Adapter_DbTableTest extends PHPUnit_Framework_TestCase
     {
         //$adapter = new Centurion_Auth_Adapter_DbTable($dbAdapter, 'auth_user', 'password');
 
+        Centurion_Auth::getInstance()->setStorage(new Zend_Auth_Storage_NonPersistent());
+        
         $validator = new Auth_Form_Validator_Login(array(
                                                         'dbAdapter'         =>  Zend_Db_Table_Abstract::getDefaultAdapter(),
                                                         'tableName'         =>  'auth_user',
