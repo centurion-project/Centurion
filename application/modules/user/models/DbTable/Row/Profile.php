a7ee9dac081ce1eda506f72b3346b7d7ff250c3d
Laurent <lc@centurion-project.org>
2012-05-04 15:00:34 +0200

    Add empty function getAvatar It return px by default, but it's here to be extend in your different project by your need.

--- application/modules/user/models/DbTable/Row/Profile.php
+++ application/modules/user/models/DbTable/Row/Profile.php
@@ -3,6 +3,11 @@ class User_Model_DbTable_Row_Profile extends Centurion_Db_Table_Row_Abstract
 {
     public function __toString()
     {
-        return $this->nickname;
+        return $this->user->username;
+    }
+
+    public function getAvatar()
+    {
+        return $this->px;
     }
 }
