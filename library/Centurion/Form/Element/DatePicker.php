40f19546cad90e835f86cb4d12baa8f323ca22c3
Laurent <lc@centurion-project.org>
2012-06-22 10:35:27 +0200

    Rename in camel case.

---
+++ library/Centurion/Form/Element/DatePicker.php
@@ -0,0 +1,6 @@
+<?php
+
+class Centurion_Form_Element_DatePicker extends Zend_Form_Element_Text
+{
+    
+}
--- library/Centurion/Form/Element/Datepicker.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Centurion_Form_Element_Datepicker extends Zend_Form_Element_Text
-{
-    
-}
