15dcb3f59b271daa8483613cbc6ea1a36359e066
Florent Jaby <florent.jaby@gmail.com>
2012-06-22 11:56:45 +0200

    fix typo

--- library/Centurion/Tool/Project/Provider/Db.php
+++ library/Centurion/Tool/Project/Provider/Db.php
@@ -242,7 +242,7 @@ class Centurion_Tool_Project_Provider_Db extends Centurion_Tool_Project_Provider
                         }
                     }
 
-                    touch($file->getPathname() '.' . APPLICATION_ENV . '.done');
+                    touch($file->getPathname() . '.' . APPLICATION_ENV . '.done');
                     echo "\n\n";
                 }
             }
